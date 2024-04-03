27796ca3b0215f2640094acb9574afabf96be3f6
Alina Rudanets <82982601+AllinaArr@users.noreply.github.com>
2024-04-03 11:10:11 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(a) {
+  cats.push(a);
+}
+
+function destructivelyPrependCat(a) {
+  cats.unshift(a);
+}
+
+function destructivelyRemoveLastCat(a) {
+  cats.pop(a);
+}
+
+function destructivelyRemoveFirstCat(a) {
+  cats.shift(a);
+}
+
+function appendCat(a) {
+  return [...cats, a];
+}
+
+function prependCat(a) {
+  return [a, ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
